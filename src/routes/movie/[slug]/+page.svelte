e28923f35100b44480d31fa84d2860505a260195
Nicholas Voelker <volkoff223@gmail.com>
2023-01-18 10:57:17 -0500

    Added movie info page with image.

--- src/routes/movie/[slug]/+page.svelte
+++ src/routes/movie/[slug]/+page.svelte
@@ -1,7 +1,17 @@
 <script>
+	import BgImage from '../../../components/BGImage.svelte';
+
 	export let data;
 	let movieDetails = data.props.movieDetails;
 	console.log(movieDetails);
 </script>
 
-<h1>{movieDetails.title}</h1>
+<div class="container w-full">
+	<div class=" -z-50 w-4/5 mx-auto">
+		<BgImage src={`https://image.tmdb.org/t/p/w780${movieDetails.backdrop_path}`} />
+		<div class="text-6xl font-bold text-center">{movieDetails.title}</div>
+		<div class="text-3xl font-bold text-center">"{movieDetails.tagline}"</div>
+		<div class="text-lg">{movieDetails.overview}</div>
+		<div class="text-md">{movieDetails.vote_average}</div>
+	</div>
+</div>
